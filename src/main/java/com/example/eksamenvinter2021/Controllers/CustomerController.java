48ff61adf38716fb6dc4877f824baccc62c5808f
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2021-11-26 14:02:09 +0100

    Added Customer Repository, service & model

--- src/main/java/com/example/eksamenvinter2021/Controllers/CustomerController.java
+++ src/main/java/com/example/eksamenvinter2021/Controllers/CustomerController.java
@@ -1,4 +1,10 @@
 package com.example.eksamenvinter2021.Controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CustomerController {
+
+
+
 }
--- src/main/java/com/example/eksamenvinter2021/Services/CustomerService.java
+++ src/main/java/com/example/eksamenvinter2021/Services/CustomerService.java
@@ -1,4 +1,31 @@
 package com.example.eksamenvinter2021.Services;
 
+import com.example.eksamenvinter2021.Models.Customer;
+import com.example.eksamenvinter2021.Resporsitories.CustomerRepo;
+
+import java.util.Date;
+
 public class CustomerService {
+
+    CustomerRepo customerRepo = new CustomerRepo();
+
+    public void createNewCustomer(String customerName) {
+        Customer customer = new Customer(customerName);
+        customerRepo.insertCustomerIntoDatabase(customer);
+    }
+
+    public void updateCustomer(Customer customer, String customerName) {
+        customer.setCustomerName(customerName);
+        customerRepo.updateCustomerInDatabase(customer);
+    }
+
+    public void deleteCustomer(int customerId){
+        customerRepo.deleteCustomerFromDatabase(customerId);
+    }
+
+
+    public Customer showProject (int customerId) {
+        return customerRepo.getCustomerFromDatabase(customerId);
+    }
+
 }

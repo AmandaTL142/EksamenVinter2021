df5c0cd761d5616840e0d098b432f0178faf8dcb
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2021-11-26 19:06:52 +0100

    Added Employee Repository, service & model

--- src/main/java/com/example/eksamenvinter2021/Controllers/CustomerController.java
+++ src/main/java/com/example/eksamenvinter2021/Controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.example.eksamenvinter2021.Controllers;
 
+import com.example.eksamenvinter2021.Services.CustomerService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CustomerController {
+
+    CustomerService cs = new CustomerService();
 
 
 
--- src/main/java/com/example/eksamenvinter2021/Services/EmployeeService.java
+++ src/main/java/com/example/eksamenvinter2021/Services/EmployeeService.java
@@ -1,4 +1,30 @@
 package com.example.eksamenvinter2021.Services;
 
+import com.example.eksamenvinter2021.Models.Employee;
+import com.example.eksamenvinter2021.Resporsitories.EmployeeRepo;
+
 public class EmployeeService {
+
+    EmployeeRepo employeeRepo = new EmployeeRepo();
+
+    public void createNewEmployee(String employeeName, String competence) {
+        Employee employee = new Employee(employeeName, competence);
+        employeeRepo.insertEmployeeIntoDatabase(employee);
+    }
+
+    public void updateEmployee(Employee employee, String employeeName, String competence) {
+        employee.setEmployeeName(employeeName);
+        employee.setCompetence(competence);
+        employeeRepo.updateEmployeeInDatabase(employee);
+    }
+
+    public void deleteCustomerFromDatabase(int employeeId){
+        employeeRepo.deleteEmployeeFromDatabase(employeeId);
+    }
+
+
+    public Employee showEmployee (int employeeId) {
+        return employeeRepo.getEmployeeFromDatabase(employeeId);
+    }
+
 }

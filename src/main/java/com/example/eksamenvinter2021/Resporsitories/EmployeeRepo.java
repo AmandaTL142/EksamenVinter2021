b566cd48544d3deab4a95c2d2766f913ae639e30
Christian Hundahl <chri02jx@stud.kea.dk>
2021-11-30 15:02:22 +0100

    Log-in updates Log-in controller updates fixes in EmployeeRepo - now showEmployee gets employee correctly

--- src/main/java/com/example/eksamenvinter2021/Resporsitories/EmployeeRepo.java
+++ src/main/java/com/example/eksamenvinter2021/Resporsitories/EmployeeRepo.java
@@ -11,7 +11,7 @@ public class EmployeeRepo {
     public void insertEmployeeIntoDatabase(Employee employee) {
         try {
             PreparedStatement stmt = JDBC.getConnection().prepareStatement
-                    ("INSERT INTO heroku_7aba49c42d6c0f0.employee (`name`,`competence`) " + "VALUES (?,?);");
+                    ("INSERT INTO heroku_7aba49c42d6c0f0.employee (`name`,`competence`) VALUES (?,?);");
             stmt.setString(1, employee.getEmployeeName());
             stmt.setString(2, employee.getCompetence());
             stmt.executeUpdate();
@@ -23,7 +23,7 @@ public class EmployeeRepo {
     }
     public Employee getEmployeeFromDatabase(int id) {//TODO: Se igennem og ryd op for unødvendig kode
         Employee employee = new Employee();
-        //String name = "";
+        String name = "";
         String competence = "";
         String password = "";
         String role = "";
@@ -32,12 +32,12 @@ public class EmployeeRepo {
                     "SELECT * FROM heroku_7aba49c42d6c0f0.employees WHERE employee_id=?;");
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
-            //while (rs.next()) { //TODO: Ikke nødvendigt da rs kun arbejder med én linje
-            String name = rs.getString("name");
+            while (rs.next()) {
+                name = rs.getString("name");
                 competence = rs.getString("competence");
                 password = rs.getString("password");
                 role = rs.getString("role");
-            //}
+            }
             employee = new Employee(name, password, competence, role);
 
         } catch(SQLException e){

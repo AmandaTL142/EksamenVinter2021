941c8a0f37d5996dc5a72c08afacc6c30949411e
Casper <casper.aakviststeengaard@gmail.com>
2021-12-10 10:16:18 +0100

    Gantt

--- src/main/java/com/example/eksamenvinter2021/Controllers/GanttController.java
+++ src/main/java/com/example/eksamenvinter2021/Controllers/GanttController.java
@@ -1,12 +1,20 @@
 package com.example.eksamenvinter2021.Controllers;
 
 import com.example.eksamenvinter2021.Resporsitories.ProjectRepo;
+import com.example.eksamenvinter2021.Services.LoginService;
+import com.example.eksamenvinter2021.Services.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class GanttController {
     ProjectRepo pr = new ProjectRepo();
+    ProjectService ps = new ProjectService();
+    LoginService ls = new LoginService();
 
     @GetMapping("/gantt")
     public String gant(Model model) {
@@ -14,6 +22,23 @@ public class GanttController {
         model.addAttribute("projects", pr.getProjectsInArrayForGantt());
         return "/Gantt-chart";
     }
+
+
+
+    /*
+    //Denne virker
+    @GetMapping("/gantt/{thisProject}")
+    public String ganttProject(@PathVariable("thisProject") int thisProject, Model model, HttpSession session) {
+        if (ls.notLoggedIn(session)) {
+            return  "redirect:/";
+        } else {
+            //int id = Integer.parseInt(thisProject);
+            model.addAttribute("Project", ps.getProjectObject(thisProject));
+            return "/Gantt-chart";
+        }
+    }
+
+     */
 }
 
 

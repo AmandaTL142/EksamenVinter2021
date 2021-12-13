2bc0b57a97d40667ccdedafa63faa505be370e69
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2021-12-13 20:59:31 +0100

    gantt og CSS

--- src/main/java/com/example/eksamenvinter2021/Controllers/GanttController.java
+++ src/main/java/com/example/eksamenvinter2021/Controllers/GanttController.java
@@ -1,8 +1,10 @@
 package com.example.eksamenvinter2021.Controllers;
 
+import com.example.eksamenvinter2021.Models.Employee;
 import com.example.eksamenvinter2021.Resporsitories.ProjectRepo;
 import com.example.eksamenvinter2021.Services.LoginService;
 import com.example.eksamenvinter2021.Services.ProjectService;
+import com.example.eksamenvinter2021.Services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ public class GanttController {
     ProjectRepo pr = new ProjectRepo();
     ProjectService ps = new ProjectService();
     LoginService ls = new LoginService();
+    TaskService ts = new TaskService();
 
     @GetMapping("/gantt")
     public String gant(Model model, HttpSession session) {
@@ -21,7 +24,6 @@ public class GanttController {
             return "redirect:/";
         } else {
             model.addAttribute("projects", ps.getAllProjectsAndSubprojects());
-            System.out.println(ps.getAllProjectsAndSubprojects().toString());
 
             return "/Gantt-chart";
         }

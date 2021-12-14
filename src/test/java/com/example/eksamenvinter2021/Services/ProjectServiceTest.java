0628ecd56c444a426eb1df62e049a33c3f511f48
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-12-14 11:52:17 +0100

    Den første unit-test.

---
+++ src/test/java/com/example/eksamenvinter2021/Services/ProjectServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.eksamenvinter2021.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void createNewProjectObject() {
+    }
+
+    @Test
+    void updateProject() {
+    }
+
+    @Test
+    void getProjectObject() {
+    }
+
+    @Test
+    void deleteProjectFromDatabase() {
+    }
+
+    @Test
+    void getAllProjectsAndSubprojects() {
+    }
+}

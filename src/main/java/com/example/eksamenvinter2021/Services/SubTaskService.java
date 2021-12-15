6d6097347179816627c33724da1d8d02a52d723e
andreajiao <andreajiao@gmail.com>
2021-12-15 06:38:40 +0100

    subtask service

--- src/main/java/com/example/eksamenvinter2021/Services/SubTaskService.java
+++ src/main/java/com/example/eksamenvinter2021/Services/SubTaskService.java
@@ -1,4 +1,28 @@
 package com.example.eksamenvinter2021.Services;
 
+import com.example.eksamenvinter2021.Models.SubTask;
+import com.example.eksamenvinter2021.Resporsitories.SubTaskRepo;
+
 public class SubTaskService {
+    SubTaskRepo sr = new SubTaskRepo();
+
+    public SubTask createNewSubtask( String title, String description, String estimatedTime, String timeUsed, String status,String startDate, String endDate){
+        SubTask sb = new SubTask(title,description,estimatedTime,timeUsed,status,startDate,endDate);
+
+        return sb;
+    }
+
+    public void updateSubtask(){
+
+    }
+
+    public SubTask getSubtaskObject(int subtaskID){
+        return sr.getSubtaskFromDB(subtaskID);
+    }
+
+    public void deleteSubtaskFromDB(int id){
+        sr.deleteSubtaskFromDB(id);
+    }
+
+
 }

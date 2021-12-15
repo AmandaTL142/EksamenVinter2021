3f6f4e4971b699701b3012bfc3ac05b53144366d
andreajiao <andreajiao@gmail.com>
2021-12-15 06:30:42 +0100

    task service

--- src/main/java/com/example/eksamenvinter2021/Services/TaskService.java
+++ src/main/java/com/example/eksamenvinter2021/Services/TaskService.java
@@ -12,35 +12,36 @@ public class TaskService {
 
     TaskRepo tr = new TaskRepo();
 
-    public Task createNewTask(String title, String description, String estimatedTime, String timeUsed, String status) {
-        Task task = new Task(title, description, estimatedTime, timeUsed, status);
-        //tr.insertNewTaskToDB(task);
+
+    public Task createNewTask(String title, String description, String estimatedTime, String timeUsed, String status, String startDate, String endDate) {
+
+
+        Task task = new Task(title, description, estimatedTime, timeUsed, status, startDate, endDate);
+
         return task;
     }
 
-    public Task updateTask(String title, String description, String estimatedTime, String timeUsed, String status) {
-        Task t = new Task(title, description, estimatedTime, timeUsed, status);
-        tr.updateTask(t);
-
-        return t;
-
+    public void insertNewTaskToDB(Task task){
+        tr.insertNewTaskToDB(task);
     }
 
 
-    public ArrayList<Task> getAllTasks(int taskID){
-        return tr.getAllTasksInnProject(taskID);
+    public void updateTask(Task t) {
+
+        tr.updateTask(t);
     }
 
     public Task getTaskObject(int taskId) {
         return tr.getTaskFromDB(taskId);
     }
 
+
+    public ArrayList<Task> getAllTasksInArray(){
+        return tr.getTasksInArray();
+    }
+
     public void deleteTask(int taskID){
         tr.deleteTask(taskID);
     }
 
-    public ArrayList<Task> showTaskLinkedToProject(int thisProject){
-        return tr.getTasksInArray();
-    }
 }
-

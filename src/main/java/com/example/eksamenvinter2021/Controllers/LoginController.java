fdb692a6e0679e19d24f1a9178ff3ecdeb66d3eb
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2021-12-16 18:57:26 +0100

    Tilbage

--- src/main/java/com/example/eksamenvinter2021/Controllers/LoginController.java
+++ src/main/java/com/example/eksamenvinter2021/Controllers/LoginController.java
@@ -38,7 +38,7 @@ public class LoginController {
             Employee employee = es.showEmployee(employee_id);
             //Sæt employee som attribut til sessionen
             session.setAttribute("employee", employee);
-            return "/frontpage";
+            return "redirect:/frontpage";
         }
         return "index";
     }

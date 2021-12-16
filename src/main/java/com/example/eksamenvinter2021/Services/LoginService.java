3543e920b1d8d67cc7ef726c94ea48ec3569789d
Christian Hundahl <chri02jx@stud.kea.dk>
2021-12-16 15:07:54 +0100

    Fjernede ubrugte metoder i LoginService.java

--- src/main/java/com/example/eksamenvinter2021/Services/LoginService.java
+++ src/main/java/com/example/eksamenvinter2021/Services/LoginService.java
@@ -1,34 +1,19 @@
 package com.example.eksamenvinter2021.Services;
 
-import com.example.eksamenvinter2021.Models.Project;
 import com.example.eksamenvinter2021.Resporsitories.EmployeeRepo;
-import com.example.eksamenvinter2021.Utility.JDBC;
 import javax.servlet.http.HttpSession;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Date;
 
 public class LoginService {
     //Christian Hundahl
-    //TODO:
-    //LoginService should contact database via repository, not in this class
-    //This will be implemented when EmployeeRepo finished
+
     public static boolean login(int employee_id, String password) {
         return EmployeeRepo.login(employee_id, password);
     }
 
+    //Evaluerer om bruger er manager
+    //Benyttes ikke i projektet, men kan finde anvendelse på senere tidspunkt
     public static boolean isManager(int userId) {
         return EmployeeRepo.isManager (userId);
-    }
-
-    public static boolean validLogin(HttpSession session) {
-        //All GetMappings implements validLogin method to find if user is logged in
-        return session.getAttribute("id") != null;
-    }
-
-    public static boolean validManagerLogin(HttpSession session) {
-        return (boolean) session.getAttribute("manager");
     }
 
     public static boolean notLoggedIn(HttpSession session) {
